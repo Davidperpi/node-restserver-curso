9d529ff7b1bcf1f0d28b1459d196e37359123ced
David <davidperpi7@gmail.com>
2020-11-28 20:15:52 +0100

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,9 +13,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =================== 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://admin:ea5Xtmrpxk8xEjKn@cluster0.jgfdg.mongodb.net/cafe';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 process.env.URLDB = urlDB;
